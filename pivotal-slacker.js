f0c51d1c18cd81221bd7f800dba2cbf20cba0709
Brian Kelly <polymonic@gmail.com>
2015-03-11 07:34:21 -0400

    Initial commit

---
+++ pivotal-slacker.js
@@ -0,0 +1,24 @@
+var Slack = require('slack-client');
+var dotenv = require('dotenv');
+dotenv.load();
+
+var token = process.env.SLACK_TOKEN;
+var autoReconnect = true;
+var autoMark = true;
+
+var slack = new Slack(token, autoReconnect, autoMark);
+
+slack.on('open', function() {
+  console.log("CONNECTED!");
+});
+
+slack.on('message', function(message) {
+  channel = slack.getChannelGroupOrDMByID(message.channel)
+  channel.send("Yo yo yo!");
+});
+
+slack.on('error', function(error) {
+  console.error(error.msg);
+});
+
+slack.login();
